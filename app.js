8a40904a3c2e23d680144fbfb1940718511c0a18
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-10-15 16:11:22 +0530

    Conversion to other weights done successfully

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ const lbsInput = document.getElementById('lbsInput');
 lbsInput.addEventListener('input', convertWeight);
 
 function convertWeight(e) {
+    const lbs = e.target.value;
+    const gramsOutput = document.getElementById('gramsOutput');
+    const kgOutput = document.getElementById('kgOutput');
+    const ozOutput = document.getElementById('ozOutput');
 
+    gramsOutput.innerHTML = lbs / 0.0022046;
+    kgOutput.innerHTML = lbs / 2.2046;
+    ozOutput.innerHTML = lbs * 16;
 }
